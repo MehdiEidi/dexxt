952851db20b952ab28dfa81f636f854473620d72
Mehdi Eidi <mehdiadq@gmail.com>
2022-10-02 22:09:31 +0330

    fix: rename stuff. testing for vercel

--- api/index.go
+++ api/index.go
@@ -21,63 +21,63 @@ const (
 
 var telegramAPI string = TELEGRAM_API_BASE_URL + os.Getenv(BOT_TOKEN_ENV) + TELEGRAM_API_SEND_MESSAGE
 
-// Update is a Telegram object that we receive every time a user interacts with the bot.
-type Update struct {
+// update is a Telegram object that we receive every time a user interacts with the bot.
+type update struct {
 	UpdateId int     `json:"update_id"`
-	Message  Message `json:"message"`
+	Message  message `json:"message"`
 }
 
 // String implements the fmt.String interface to get the representation of an Update as a string.
-func (u Update) String() string {
+func (u update) String() string {
 	return fmt.Sprintf("(update id: %d, message: %s)", u.UpdateId, u.Message)
 }
 
-// Message is a Telegram object that can be found in an update.
-type Message struct {
+// message is a Telegram object that can be found in an update.
+type message struct {
 	Text     string   `json:"text"`
-	Chat     Chat     `json:"chat"`
-	Audio    Audio    `json:"audio"`
-	Voice    Voice    `json:"voice"`
-	Document Document `json:"document"`
+	Chat     chat     `json:"chat"`
+	Audio    audio    `json:"audio"`
+	Voice    voice    `json:"voice"`
+	Document document `json:"document"`
 }
 
 // String implements the fmt.String interface to get the representation of a Message as a string.
-func (m Message) String() string {
+func (m message) String() string {
 	return fmt.Sprintf("(text: %s, chat: %s, audio %s)", m.Text, m.Chat, m.Audio)
 }
 
-// Audio refer to a audio file sent.
-type Audio struct {
+// audio refer to a audio file sent.
+type audio struct {
 	FileId   string `json:"file_id"`
 	Duration int    `json:"duration"`
 }
 
 // String implements the fmt.String interface to get the representation of an Audio as a string.
-func (a Audio) String() string {
+func (a audio) String() string {
 	return fmt.Sprintf("(file id: %s, duration: %d)", a.FileId, a.Duration)
 }
 
-// Voice can be summarized with similar attribute as an Audio message for our use case.
-type Voice Audio
+// voice can be summarized with similar attribute as an Audio message for our use case.
+type voice audio
 
-// Document refer to a file sent.
-type Document struct {
+// document refer to a file sent.
+type document struct {
 	FileId   string `json:"file_id"`
 	FileName string `json:"file_name"`
 }
 
 // String implements the fmt.String interface to get the representation of an Document as a string.
-func (d Document) String() string {
+func (d document) String() string {
 	return fmt.Sprintf("(file id: %s, file name: %s)", d.FileId, d.FileName)
 }
 
-// Chat indicates the conversation to which the Message belongs.
-type Chat struct {
+// chat indicates the conversation to which the Message belongs.
+type chat struct {
 	ID int `json:"id"`
 }
 
 // String implements the fmt.String interface to get the representation of a Chat as a string.
-func (c Chat) String() string {
+func (c chat) String() string {
 	return fmt.Sprintf("(id: %d)", c.ID)
 }
 
@@ -99,8 +99,8 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 // parseIncomingRequest parses incoming update to Update.
-func parseIncomingRequest(r *http.Request) (*Update, error) {
-	var update Update
+func parseIncomingRequest(r *http.Request) (*update, error) {
+	var update update
 
 	if err := json.NewDecoder(r.Body).Decode(&update); err != nil {
 		log.Printf("could not decode incoming update %s", err.Error())
